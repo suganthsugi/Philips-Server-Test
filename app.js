e17a7ea99f6ef8d5b0c2152f6175f8d70f022119
suganthsugi <suganthjayanthi@gmail.com>
2023-02-22 12:01:23 +0530

    initialized authorization

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 app.use('/accounts', userRoute);
 
+
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server started at port http://localhost:${port}`);
